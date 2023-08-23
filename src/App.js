c38a43604a0ce77d741ccf8d5ea27fd99bebd8ca
Hassan Sajjad <sajjadhassa389@gmail.com>
2023-08-23 22:18:23 +0500

    feature/Initial_design_testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Login from "./components/Login/Login";
+import Login from "./components/Login/login";
+import Signup from "./components/Signup/Signup";
+import Verification from "./components/Verification/Verification";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/nav" element={<Navbar />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/otp" element={<Verification />} />
       </Routes>
     </BrowserRouter>
   );
