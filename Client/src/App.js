9069456a074045355db8a8662428c5b7f36003c1
Hassan Sajjad <sajjadhassa389@gmail.com>
2023-09-21 16:12:42 +0500

    Fix/Final_Changes

--- Client/src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-import Routers from "./routes/Routers";
-function App() {
-  return <Routers />;
-}
-
-export default App;
--- src/components/Navbar2/Logo.js
+++
@@ -1,5 +0,0 @@
-import edfry from "assets/svgs/Group 5086.png";
-export const Logo = () => (
-    <img src={edfry}></img>
-
-);
